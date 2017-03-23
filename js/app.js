8ef1fbd6e76df5c5283cd570f0916e7b64ef6c8e
Mateusz Mazurkiewicz <macos@Mazur.local>
2017-03-23 18:51:15 +0100

    slider, drop down menu, hover effects

---
+++ js/app.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM");
+
+    var next = document.querySelector("#nextPicture");
+    var prev = document.querySelector("#prevPicture");
+    var allPics = document.querySelectorAll(".fourty ul li");
+    var picNumber = 0;
+
+    allPics[picNumber].classList.toggle("visible");
+
+    next.addEventListener("click", function() {
+        console.log("next click");
+
+        allPics[picNumber].classList.toggle("visible");
+        picNumber++;
+
+        if (picNumber >= allPics.length) {
+            picNumber = 0;
+        }
+        allPics[picNumber].classList.toggle("visible");
+    });
+
+    prev.addEventListener("click", function() {
+        console.log("prev click");
+        allPics[picNumber].classList.toggle("visible");
+        picNumber--;
+        if (picNumber < 0) {
+            picNumber = allPics.length - 1;
+        }
+        allPics[picNumber].classList.toggle("visible");
+    });
+
+
+})
