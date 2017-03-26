fd55f9327ef12655380931723fd816db3efcbde8
Mateusz Mazurkiewicz <macos@Mazur.local>
2017-03-26 18:07:56 +0200

    slider animations

--- js/app.js
+++ js/app.js
@@ -6,27 +6,38 @@ document.addEventListener("DOMContentLoaded", function() {
     var allPics = document.querySelectorAll(".fourty ul li");
     var picNumber = 0;
 
+    allPics[picNumber].classList.toggle("animated");
+    allPics[picNumber].classList.toggle("fadeIn");
     allPics[picNumber].classList.toggle("visible");
+
 
     next.addEventListener("click", function() {
         console.log("next click");
-
+        allPics[picNumber].classList.toggle("animated");
+        allPics[picNumber].classList.toggle("fadeIn");
         allPics[picNumber].classList.toggle("visible");
         picNumber++;
 
         if (picNumber >= allPics.length) {
             picNumber = 0;
         }
+
+        allPics[picNumber].classList.toggle("animated");
+        allPics[picNumber].classList.toggle("fadeIn");
         allPics[picNumber].classList.toggle("visible");
     });
 
     prev.addEventListener("click", function() {
         console.log("prev click");
+        allPics[picNumber].classList.toggle("animated");
+        allPics[picNumber].classList.toggle("fadeIn");
         allPics[picNumber].classList.toggle("visible");
         picNumber--;
         if (picNumber < 0) {
             picNumber = allPics.length - 1;
         }
+        allPics[picNumber].classList.toggle("animated");
+        allPics[picNumber].classList.toggle("fadeIn");
         allPics[picNumber].classList.toggle("visible");
     });
 
